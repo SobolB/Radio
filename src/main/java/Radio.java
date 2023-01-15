fcecde0b15cb47fe37b4d35cb21984ed96a78d51
Soboleva Ekaterina <soboleva.ketti0403@gmail.com>
2023-01-15 18:02:43 +0300

    2

--- src/main/java/Radio.java
+++ src/main/java/Radio.java
@@ -1,9 +1,16 @@
 public class Radio {
     private int currentStation;
     private int currentVolume;
+    private int maxStation;
 
+    public Radio(){
+        maxStation = 9;
+    }
+    public Radio(int stationsCount){
+        maxStation=stationsCount-1;
+    }
     public void next(){
-        if (currentStation<9){
+        if (currentStation<maxStation){
             currentStation++;
         }else{
             currentStation = 0;
@@ -13,7 +20,7 @@ public class Radio {
         if(currentStation >0){
             currentStation--;
         }else{
-            currentStation=9;
+            currentStation=maxStation;
         }
     }
 
@@ -25,7 +32,7 @@ public class Radio {
         if (currentStation < 0){
             return;
         }
-        if (currentStation > 9){
+        if (currentStation > maxStation){
             return;
         }
         this.currentStation = currentStation;
--- src/test/java/RadioTest.java
+++ src/test/java/RadioTest.java
@@ -4,12 +4,13 @@ import org.junit.jupiter.api.Test;
 class RadioTest {
     @Test
     public void test(){
-        Radio radio = new Radio();
-        radio.setCurrentStation(9);
+        Radio radio = new Radio(30);
+
+        radio.setCurrentStation(18);
 
         radio.next();
 
-        int expected = 0;
+        int expected = 19;
         int actual = radio.getCurrentStation();
 
         Assertions.assertEquals(expected, actual);
