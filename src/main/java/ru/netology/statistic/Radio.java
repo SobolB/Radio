c80d3c12fcfa1292ef689e4510a05171a40dc41d
SobolB <79850869+SobolB@users.noreply.github.com>
2023-01-17 19:40:26 +0300

    Update Radio.java

--- src/main/java/ru/netology/statistic/Radio.java
+++ src/main/java/ru/netology/statistic/Radio.java
@@ -4,37 +4,61 @@ public class Radio {
     private int currentStation;
     private int currentVolume;
 
+
+    public void next(){
+        if (currentStation<9){
+            currentStation= currentStation+1;
+            return;
+        }
+        currentStation=0;
+    }
+    public void prev(){
+        if(currentStation >0){
+            currentStation= currentStation-1;
+        }else{
+            currentStation = 9;
+        }
+    }
+
     public int getCurrentStation() {
         return currentStation;
     }
 
-    public void next() {
-        if (currentStation < 9) {
-            currentStation = currentStation + 1;
-        }
-    }
-
-    public void prev() {
-        if (currentStation > 0) {
-            currentStation = currentStation - 1;
-        }
-    }
-
     public void setCurrentStation(int currentStation) {
-        if (currentStation < 0) {
+        if (currentStation < 0){
+            return;
         }
-        if (currentStation > 9) {
+        if (currentStation > 9){
+            return;
         }
         this.currentStation = currentStation;
     }
 
+
     public int getCurrentVolume() {
         return currentVolume;
+    }
+
+    public void setCurrentVolume(int currentVolume) {
+        if (currentVolume < 0){
+            return;
+        }
+        if (currentVolume > 10){
+            return;
+        }
+        this.currentVolume = currentVolume;
     }
 
     public void increaseVolume() {
         if (currentVolume < 10) {
             currentVolume = currentVolume + 1;
         }
+    }
+
+    public void reduceVolume(){
+        if(currentVolume >0){
+            currentVolume = currentVolume - 1;
+        }
+
     }
 }
